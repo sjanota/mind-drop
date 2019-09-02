bbf351c796de68af56127c84fb53a9377c736b61
Szymon Janota <szymon.janota@sap.com>
2019-09-02 07:32:01 +0200

    Remove labels

--- src/components/IdeasDashboard/ChangeIdeaModal/ChangeIdeaModal.js
+++ src/components/IdeasDashboard/ChangeIdeaModal/ChangeIdeaModal.js
@@ -6,6 +6,14 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Badge from "react-bootstrap/Badge";
+import Octicon, {X} from "@primer/octicons-react";
+
+function RemovableLabel({label, onDelete}) {
+  return <Badge variant={"primary"} style={{margin: "6px", display: "inline-flex", alignItems: "center"}}>
+    <span style={{marginRight: '5px'}}>{label}</span>
+    <button onClick={onDelete} style={{border: 'none', background:'transparent', padding: 0, margin: 0, color: 'inherit', display: "inline-flex", alignItems: "center"}}><Octicon width={8} icon={X}/></button>
+  </Badge>
+}
 
 function MyLabelsInput({labels, setLabels}) {
   const [inputValue, setInputValue] = React.useState("");
@@ -20,8 +28,12 @@ function MyLabelsInput({labels, setLabels}) {
     }
   };
 
+  const onDeleteLabel = (label) => () => {
+    setLabels(old => [...old.slice(0, old.indexOf(label)), ...old.slice(old.indexOf(label) + 1, old.length)])
+  };
+
   return <div className={"form-control"} style={{display: "flex", alignItems: "center", flexWrap: 'wrap', padding: 0, height: 'auto'}}>
-    {labels.map(l => <Badge variant={"primary"} key={l} style={{margin: "6px"}}>{l}</Badge>)}
+    {labels.map(l => <RemovableLabel label={l} key={l} onDelete={onDeleteLabel(l)}/>)}
     <input type='text' value={inputValue} onChange={onChange} className={"form-control"} style={{width: 'auto', flexGrow: 2, border: 'none', background: 'transparent', minWidth: '30px'}}/>
   </div>
 }
