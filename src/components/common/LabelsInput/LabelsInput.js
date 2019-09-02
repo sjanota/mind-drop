88a1efbbd7552a548ec5f7ea8d6bf69854b0d044
Szymon Janota <szymon.janota@sap.com>
2019-09-02 08:04:18 +0200

    Fix labels styling

--- src/components/common/LabelsInput/LabelsInput.js
+++ src/components/common/LabelsInput/LabelsInput.js
@@ -4,9 +4,9 @@ import Badge from "react-bootstrap/Badge";
 import Octicon, {X} from "@primer/octicons-react";
 
 function RemovableLabel({label, onDelete}) {
-  return <Badge variant={"primary"} style={{margin: "6px", display: "inline-flex", alignItems: "center"}}>
-    <span style={{marginRight: '5px'}}>{label}</span>
-    <button onClick={onDelete} style={{border: 'none', background:'transparent', padding: 0, margin: 0, color: 'inherit', display: "inline-flex", alignItems: "center"}}><Octicon width={8} icon={X}/></button>
+  return <Badge className={"RemovableLabel"} variant={"primary"}>
+    <span>{label}</span>
+    <button onClick={onDelete}><Octicon width={8} icon={X}/></button>
   </Badge>
 }
 
@@ -27,9 +27,9 @@ function LabelsInput({labels, setLabels}) {
     setLabels(old => [...old.slice(0, old.indexOf(label)), ...old.slice(old.indexOf(label) + 1, old.length)])
   };
 
-  return <div className={"form-control"} style={{display: "flex", alignItems: "center", flexWrap: 'wrap', padding: 0, height: 'auto'}}>
+  return <div className={"form-control LabelsInput"}>
     {labels.map(l => <RemovableLabel label={l} key={l} onDelete={onDeleteLabel(l)}/>)}
-    <input type='text' value={inputValue} onChange={onChange} className={"form-control"} style={{width: 'auto', flexGrow: 2, border: 'none', background: 'transparent', minWidth: '30px'}}/>
+    <input type='text' value={inputValue} onChange={onChange} className={"form-control"}/>
   </div>
 }
 
