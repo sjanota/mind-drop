ff1ea015a051a977f435dd77e6ab6e49a6d36887
Szymon Janota <szymon.janota@sap.com>
2019-09-03 21:34:59 +0200

    Add AddIdeaCard test

--- src/components/IdeasDashboard/AddIdeaCard/AddIdeaCard.test.js
+++ src/components/IdeasDashboard/AddIdeaCard/AddIdeaCard.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AddIdeaCard from './AddIdeaCard';
+import {mount} from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<AddIdeaCard />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('calls onClick when button is clicked', () => {
+  const onClick = jest.fn();
+  const component = mount(<AddIdeaCard onClick={onClick}/>);
+  component.find('button').simulate('click');
+  expect(onClick).toBeCalledTimes(1);
 });
