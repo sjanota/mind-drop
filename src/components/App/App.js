42559d30c45a873337f4cb1d9f8174c6ed9544a0
Szymon Janota <szymon.janota@sap.com>
2019-09-08 18:01:40 +0200

    Rename Cards import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 import Navbar from "../Navbar/Navbar";
-import Cards from "../IdeasDashboard/IdeasDashboard";
+import IdeasDashboard from "../IdeasDashboard/IdeasDashboard";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 function App() {
   const [labelFilter, setLabelFilter] = React.useState([]);
 
-  const FilteredCards = () => <Cards labelFilter={labelFilter}/>;
+  const FilteredIdeasDashboard = () => <IdeasDashboard labelFilter={labelFilter}/>;
 
   return (
     <div className="App">
       <Navbar labelFilter={labelFilter} setLabelFilter={setLabelFilter}/>
-      <PrivateRoute component={FilteredCards}/>
+      <PrivateRoute component={FilteredIdeasDashboard}/>
     </div>
   );
 }
