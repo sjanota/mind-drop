8243a721f08b31bd3e4f4aa8f117775e5910ff82
Szymon Janota <szymon.janota@sap.com>
2019-09-09 11:55:52 +0200

    Updates

--- precache-manifest.490881fd85e43e2ae73f3253a81032ec.js
+++ precache-manifest.490881fd85e43e2ae73f3253a81032ec.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3fcd3848649fcbaeb89c1828986df8db",
+    "revision": "45c0321163ed8491a1224f99a5e3ca43",
     "url": "/index.html"
   },
   {
-    "revision": "12b56dfea98457f49854",
+    "revision": "99aca5588acd7c121f63",
     "url": "/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "0b0e58c4e410ab14cacb",
+    "revision": "2cd87711b58270a39ca5",
     "url": "/static/css/main.0e65f3f6.chunk.css"
   },
   {
-    "revision": "12b56dfea98457f49854",
-    "url": "/static/js/2.06cb6ad6.chunk.js"
+    "revision": "99aca5588acd7c121f63",
+    "url": "/static/js/2.bd0df889.chunk.js"
   },
   {
-    "revision": "0b0e58c4e410ab14cacb",
-    "url": "/static/js/main.7a52e9f6.chunk.js"
+    "revision": "2cd87711b58270a39ca5",
+    "url": "/static/js/main.5b5fa26a.chunk.js"
   },
   {
     "revision": "e144474da1cffe189c86",
