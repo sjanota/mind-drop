85875852510ab7ad1364d814670af38e2370e9d2
Szymon Janota <szymon.janota@sap.com>
2019-09-09 09:16:08 +0200

    Use MONGODB_URI evn variable

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,10 +4,18 @@ import (
 	"github.com/sjanota/mind-drop/pkg/server"
 	storage2 "github.com/sjanota/mind-drop/pkg/storage"
 	"log"
+	"os"
 )
 
+const defaultStorageUri = "mongodb://localhost:32768"
+
 func main() {
-	storage, err := storage2.New("mongodb://localhost:32768")
+	storageUri := os.Getenv("MONGODB_URI")
+	if storageUri == "" {
+		storageUri = defaultStorageUri
+	}
+
+	storage, err := storage2.New(storageUri)
 	if err != nil {
 		log.Fatalf("Cannot create storage: %s", err)
 	}
